3dfbfd24ad7517cf53a0904d3661fa16d91d341e
Praveen <sharmapraveen5749@gmail.com>
2023-10-16 14:06:37 +0530

    Code edited

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,25 @@
-public class Main {
-    public static class A{
-        public  String meth(){
+
+
+public class Main
+{
+    public static void main(String args[]){
+        B b1=new B();
+        b1.meth();
+
+        B b2=new B();
+        b2.meth();
+    }
+    public static class A
+    {
+        public String meth ()
+        {
             return "Invoking method from class A";
         }
     }
-    public static class B extends A{
-        @Override
-        public  String meth(){
+    public static class B extends A {
+        public String meth(){
+            System.out.println(super.meth());
             return "Method is overridden in Extendend class B";
-
         }
-    }
-    public static void main(String args[]){
-        B obj = new B();
-        System.out.println(obj.meth());
     }
 }
